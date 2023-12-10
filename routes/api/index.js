6041923b4818e2275f0a1334d0600d9ce420f55c
tgaytan <tgaytan14@utexas.edu>
2023-12-10 14:21:46 -0600

    created server.js, config/connection, routes/index, routes/api/index. server running with no issues

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./usersRoutes');
+const thoughtsRoutes = require('./thoughtsRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtsRoutes);
+
+module.exports = router;
