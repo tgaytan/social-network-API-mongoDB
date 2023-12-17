00678c2d850236ac616ed4d0b3ae80e81437d4f8
tgaytan <tgaytan14@utexas.edu>
2023-12-17 17:53:14 -0600

    finished UPDATED PUT for thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -19,15 +19,27 @@ module.exports = {
     },
     async createThought(req,res) {
         try {
-            res.status(200).json({ success: 'true' })
+            const thought = await Thought.create(req.body)
+            res.status(200).json(thought)
         } catch(err) {
             res.status(500).json(err)
         }
     },
     async updateThought(req,res) {
         try {
-            res.status(200).json({ success: 'true' })
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId},
+                { thoughtText: req.body.thoughtText},
+                { new: true }
+            )
+
+            if (!thought) {
+                return res.status(404).json({ message: 'thought does not exist' })
+            }
+
+            res.status(200).json(thought)
         } catch(err) {
+            console.log(err)
             res.status(500).json(err)
         }
     },
